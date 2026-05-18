58ec1365188908d63423aaacf1484c0ceaaee055
Tiago <tiagocondack@gmail.com>
2026-05-18 15:11:00 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import './App.css';
 function App() {
   return (
     <div style = {{textAlign:"center", padding:"50px"}}>
-      <h1>MInha Primeira PWA</h1>
-      <p> Agora posso criar meu próprio Progressive Web App!</p>
+      <h1>Trazendo aqui meus aprendizados e informações profissionais.</h1>
+      <p> Aqui penso em colocar minhas informações de contato e de aprendizagem.
+
+        Atualmente sou estudande de Analise e deselvolvimento de sistemas pela Unifeso, no penultimo período.
+        Aprendi bastante coisas e gostaria de compartilhar aqui minhas conquistas, <br>ainda estou planejando o que fazer por aqui, mas quero fazer algo que me demonstre como profissional e aprendiz sempre.
+        
+        SIC ITUR AD ASTRA.
+      </p>
       
     </div>
   );
